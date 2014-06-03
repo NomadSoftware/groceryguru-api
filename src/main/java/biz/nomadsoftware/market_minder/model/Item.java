aacd131f34b4666f8723f6259164ac703d0859de
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2014-06-02 21:49:59 -0400

    skeleton representations

---
+++ src/main/java/biz/nomadsoftware/market_minder/model/Item.java
@@ -0,0 +1,59 @@
+package biz.nomadsoftware.market_minder.model;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy.LowerCaseWithUnderscoresStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(LowerCaseWithUnderscoresStrategy.class)
+@JsonInclude(Include.NON_EMPTY)
+public class Item {
+
+	private Long id;
+
+	public Item() {
+		// for jackson
+	}
+
+	public Item(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id == null ? 0 : id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Item other = (Item) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
+
+}
